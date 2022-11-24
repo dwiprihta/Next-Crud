1c856674b85697753038002e5142ac2ddd9197f5
dwiprihta <prihtapambudi@gmail.com>
2022-11-24 09:19:46 +0700

    add file upload

---
+++ config/theme.js
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// Create a theme instance.
+const theme = createTheme({
+//phallete
+palette: {
+   primary: {
+      main: '#556cd6',
+   },
+   secondary: {
+     main: '#19857b',
+   },
+   error: {
+   main: red.A400,
+   },
+   white:{
+    main:'#ffff'
+   }
+  },
+
+});
+export default theme;
